77ab0479d93c765ece853ec4ead124a945489319
Abhi Yerra <abhi@berkeley.edu>
2012-01-26 02:57:14 -0800

    Learned about doall for evaluating lazy sequences.

--- test/workmachine/test/jobs.clj
+++ test/workmachine/test/jobs.clj
@@ -4,9 +4,9 @@
   (:require [workmachine.blueprints.content :as content-workflow]
             [workmachine.workflow :as workflow]))
 
-(str (content-workflow/workflow [{"topic" "http://google.com"}
-                                 {"topic" "http://yahoo.com"}
-                                 {"topic" "http://amazon.com"}]))
+(content-workflow/workflow [{"topic" "http://google.com"}
+                            {"topic" "http://yahoo.com"}
+                            {"topic" "http://amazon.com"}])
 
 (deftest test-available-jobs
   (is (not (empty? @available-jobs)) "Available jobs should have couple jobs.")
