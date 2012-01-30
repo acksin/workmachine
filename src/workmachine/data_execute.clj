84e11d79cc7236c30ce23bce054a74642c13b001
Abhi Yerra <abhi@berkeley.edu>
2012-01-29 23:58:58 -0800

    Made the functions more apparent for ocr and transcribe.

--- src/workmachine/data_execute.clj
+++ src/workmachine/data_execute.clj
@@ -1,20 +1,29 @@
 (ns workmachine.data-execute
   (:use clojure.contrib.shell-out))
 
-(defn image-ocr [name]
-  {:name name
-   :execute (fn [value]
-              (sh "convert" (str value "-type Grayscale " value ".tif"))
-              (sh "tesseract" (str value ".tif") (str "/tmp/" value ".ocr"))
-              (sh "cat" (str "/tmp/" value ".ocr.txt")))})
+(defn image-ocr [image-field text-field]
+  (fn [value]
+    (sh "convert" (str value "-type Grayscale " value ".tif"))
+    (sh "tesseract" (str value ".tif") (str "/tmp/" value ".ocr"))
+    (sh "cat" (str "/tmp/" value ".ocr.txt")))})
 
-(defn image-ocr-train [name]
-  {:name name
-   :execute (fn [value]
-              ;; http://code.google.com/p/tesseract-ocr/wiki/TrainingTesseract3
-              )})
+(defn image-ocr-train [image-field training-text-field]
+  (fn [value]
+    ;; http://code.google.com/p/tesseract-ocr/wiki/TrainingTesseract3
+    )})
 
+(defn audio-transcribe [audio-field text-field]
+  ;; TODO
+  )
+
+(defn audio-transcribe-train [audio-field training-text-field]
+  ;; TODO
+  )
+  
 (defn parse [field]
   (apply (case (first field)
-           :execute string
+           :audio-transcribe audio-transcribe
+           :audio-transcribe-train audio-transcribe-train
+           :image-ocr image-ocr
+           :image-ocr-train image-ocr-train
          (rest field))))
