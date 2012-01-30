4663a70118f12be957d5eae31750b4d2bd6bcd8a
Abhi Yerra <abhi@berkeley.edu>
2012-01-30 01:00:08 -0800

    Move the input_verify to extract and be able to specify arbitrary inputs and outputs.

---
+++ src/workmachine/blueprints/extraction.clj
@@ -0,0 +1,11 @@
+(ns workmachine.blueprints.extraction
+  (:use workmachine.workflow))
+
+(def program [inputs outputs]
+  '((:input
+     {:input [inputs]
+      :output [outputs]})))
+
+(defn workflow [inputs outputs workflow-jobs]
+  (doseq [job workflow-jobs]
+    (start-workflow (program inputs outputs) job)))
