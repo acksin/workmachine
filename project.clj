4d964eb18285ed1ffa1f603392e7854b60b001ac
Abhi Yerra <abhi@berkeley.edu>
2012-02-15 23:45:41 -0800

    Added redis.

--- project.clj
+++ project.clj
@@ -3,6 +3,7 @@
             :dependencies [[org.clojure/clojure "1.3.0"]
                            [org.clojure/clojure-contrib "1.2.0"]
                            [cheshire "2.0.2"]
-                           [noir "1.2.1"]]
+                           [noir "1.2.1"]
+                           [org.clojars.tavisrudd/redis-clojure "1.3.1-SNAPSHOT"]]
             :main workmachine.server)
 
