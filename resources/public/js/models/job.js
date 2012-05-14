ec6e17a24d3b8fa5322dff7507b7ae40fe64c2a9
Abhi Yerra <abhi@berkeley.edu>
2012-05-14 03:48:03 -0700

    backbone js demo added.

---
+++ resources/public/js/models/job.js
@@ -0,0 +1,29 @@
+(function($){
+  var JobView = Backbone.View.extend({
+    el: $('#content'),
+
+    events: {
+      'click button#add': 'addItem'
+    },
+
+    initialize: function(){
+      _.bindAll(this, 'render', 'addItem'); // every function that uses 'this' as the current object should be in here
+
+      this.counter = 0; // total number of items added thus far
+      this.render();
+    },
+    // `render()` now introduces a button to add a new list item.
+    render: function(){
+      $(this.el).append("<button id='add'>Submit</button>");
+      $(this.el).append("<ul></ul>");
+    },
+
+    // `addItem()`: Custom function called via `click` event above.
+    addItem: function(){
+      this.counter++;
+      $('ul', this.el).append("<li>hello world"+this.counter+"</li>");
+    }
+  });
+
+  var jobView = new JobView();
+})(jQuery);
